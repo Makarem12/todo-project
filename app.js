d9bdb960e07b83446341514b2694b5db5e75a128
Makarem12 <makaremabososain@gmail.com>
2024-01-19 19:29:17 +0300

    new branch added

--- app.js
+++ app.js
@@ -17,3 +17,88 @@ if(confirmMsg==false){
         alert("Welcome to our tasks website")
     }
 }
+
+function askQuestion(question) {
+    let answer = prompt(question).toLowerCase();
+    if (answer=="yes" || answer=="no") {
+        return answer;}
+        else 
+    {
+        return "invalid";
+    }
+    
+}
+
+
+function askQuestions() {
+    const answers = [];
+
+    for (let i = 1; i <= 3; i++) {
+        const question = `Question ${i}:please type Yes/No?`;
+        const answer = askQuestion(question);
+        answers.push(answer);
+    }
+
+    return answers;
+}
+
+
+function printAnswers(answers) {
+    console.log("Answers:");
+    answers.forEach((answer, index) => {
+        console.log(`Question ${index + 1}: ${answer}`);
+    });
+}
+
+
+function main() {
+    const userAnswers = askQuestions();
+    printAnswers(userAnswers);
+}
+
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
